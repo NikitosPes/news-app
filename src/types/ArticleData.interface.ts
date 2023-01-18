10ccfaae38b413577c0018edd6961aee0cbbaa81
NikitosPes <kutsokon95@gmail.com>
2023-01-18 11:41:57 +0300

    feat: create interface of article data

---
+++ src/types/ArticleData.interface.ts
@@ -0,0 +1,11 @@
+interface IArticleData {
+  id: number,
+  author: string,
+  urlToImage: string,
+  publishedAt: string
+  title: string,
+  description: string,
+  content: string
+}
+
+export default IArticleData;
