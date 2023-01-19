7c52036a8f1303ea50f3b550be68477f1f62c1b7
NikitosPes <kutsokon95@gmail.com>
2023-01-19 15:40:50 +0300

    fix: format ArticleCard props for vizualization

--- src/components/ArticlesCard/ArticlesCard.tsx
+++ src/components/ArticlesCard/ArticlesCard.tsx
@@ -1,38 +1,51 @@
-import React from "react";
+import React from 'react';
 
-import EastOutlinedIcon from "@mui/icons-material/EastOutlined";
-import CalendarTodayOutlinedIcon from "@mui/icons-material/CalendarTodayOutlined";
+import EastOutlinedIcon from '@mui/icons-material/EastOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
 
-import classes from "./ArticlesCard.module.scss";
+import classes from './ArticlesCard.module.scss';
+import IArticleData from '../../types/ArticleData.interface';
 
 interface IArticlesCardProps {
-  id: string;
-  imgURL: string;
-  publishedDate: string;
-  title: string;
-  description: string;
+  article: IArticleData;
 }
 
+const ArticlesCard = ({ article }: IArticlesCardProps) => {
+  const normilizeTextToFixedCharsNumber = (text: string, textLenth: number) => {
+    if (text.length < textLenth) return text;
+    return text.slice(0, textLenth - 3) + '...';
+  };
 
-const ArticlesCard = (props: IArticlesCardProps) => {
+  const formatPublishedDate = (date: string): string => {
+    const fromtedDate = new Date(date).toLocaleString('en-US', {
+      year: 'numeric',
+      month: 'short',
+      day: '2-digit',
+    });
+    return fromtedDate;
+  };
+
+  formatPublishedDate(article.publishedAt);
   return (
     <div className={classes.container}>
       <div className={classes.imgWrapper}>
-        <img className={classes.img} src={props.imgURL} alt="acticlePoster" />
+        <img className={classes.img} src={article.urlToImage} alt='acticlePoster' />
       </div>
 
       <div className={classes.content}>
         <div className={classes.publishedDateContainer}>
           <CalendarTodayOutlinedIcon sx={{ fontSize: 14 }} />
-          <p className={classes.publishedDate}>{props.publishedDate}</p>
+          <p className={classes.publishedDate}>{formatPublishedDate(article.publishedAt)}</p>
         </div>
 
-        <h2 className={classes.title}>{props.title}</h2>
-        
-        <div className={classes.description}>{props.description}</div>
+        <h2 className={classes.title}>{normilizeTextToFixedCharsNumber(article.title, 100)}</h2>
+
+        <div className={classes.description}>
+          {normilizeTextToFixedCharsNumber(article.description, 100)}
+        </div>
 
         <div className={classes.navLinkContainer}>
-          <a className={classes.navLink} href="#">
+          <a className={classes.navLink} href='#'>
             Read more
           </a>
           <EastOutlinedIcon sx={{ fontSize: 16 }} />
