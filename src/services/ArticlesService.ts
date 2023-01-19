9ff0f583ca3e1f2d967cdbf8258ba17241ed3193
NikitosPes <kutsokon95@gmail.com>
2023-01-19 15:29:46 +0300

    ref: separate base url from endpoints

--- src/services/ArticlesService.ts
+++ src/services/ArticlesService.ts
@@ -1,21 +1,33 @@
-import axios from "axios";
-import IArticleData from "../types/ArticleData.interface";
+import axios from 'axios';
+import IArticleData from '../types/ArticleData.interface';
 
 interface IArticlesService {
-  getArticlesBySearchQuery(searchQuery: string): Promise<IArticleData[]>;
+  getArticlesByQuery(searchQuery: string): Promise<IArticleData[]>;
+}
+
+enum ArticlesEndpoints {
+  everthing = '/v2/everything',
 }
 
 class ArticlesService implements IArticlesService {
-
-  // USED NEXT API https://newsapi.org/docs
-  private _baseURL = "https://newsapi.org/v2/everything";
-
-  async getArticlesBySearchQuery(searchQuery: string): Promise<IArticleData[]> {
-    const response = await axios.get(`${this._baseURL}?q=${searchQuery}&sortBy=popularity&apiKey=${process.env.REACT_APP_NEWS_API_KEY}`);
-    const responsedArticles: IArticleData[] = await response.data.articles;
   
+  // USED NEXT API https://newsapi.org/docs
+  private _baseURL = 'https://newsapi.org';
+
+  getFetchURL = (baseURL: string, endpoint: string) => baseURL + endpoint;
+
+  async getArticlesByQuery(query: string): Promise<IArticleData[]> {
+    const response = await axios.get(this.getFetchURL(this._baseURL, ArticlesEndpoints.everthing), {
+      params: {
+        q: query,
+        sortBy: 'popularity',
+        apiKey: process.env.REACT_APP_NEWS_API_KEY,
+      },
+    });
+    const responsedArticles: IArticleData[] = await response.data.articles;
+
     return responsedArticles;
-  };
+  }
 }
 
 export default new ArticlesService();
