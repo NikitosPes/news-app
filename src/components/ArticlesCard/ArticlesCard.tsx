13268babbda5c5616b581c565d8719c125bd81f3
NikitosPes <kutsokon95@gmail.com>
2023-01-19 18:28:29 +0300

    ref: change html structure and styles

--- src/components/ArticlesCard/ArticlesCard.tsx
+++ src/components/ArticlesCard/ArticlesCard.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-import Highlighter from "react-highlight-words";
-
-import EastOutlinedIcon from '@mui/icons-material/EastOutlined';
-import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import Highlighter from 'react-highlight-words';
 
 import ArrowLink from '../ArrowLink/ArrowLink';
+import ArticlePublishedDate from '../ArticlePublishedDate/ArticlePublishedDate';
 
 import classes from './ArticlesCard.module.scss';
 import IArticleData from '../../types/ArticleData.interface';
@@ -15,53 +13,44 @@ interface IArticlesCardProps {
   keywords: string[];
 }
 
+
 const ArticlesCard = ({ article, keywords }: IArticlesCardProps) => {
-  const normilizeTextToFixedCharsNumber = (text: string, textLenth: number) => {
-    if (text.length < textLenth) return text;
-    return text.slice(0, textLenth - 3) + '...';
+
+  const MAX_TEXT_LENGTH = 100;
+
+  const formatTextToFixedLength = (text: string, maxLength: number): string => {
+    if (text.length < maxLength) return text;
+    return text.slice(0, maxLength - 3) + '...';
   };
 
-  const formatPublishedDate = (date: string): string => {
-    const fromtedDate = new Date(date).toLocaleString('en-US', {
-      year: 'numeric',
-      month: 'short',
-      day: '2-digit',
-    });
-    return fromtedDate;
-  };
-
-  formatPublishedDate(article.publishedAt);
   return (
     <div className={classes.container}>
-      <div className={classes.imgWrapper}>
-        <img className={classes.img} src={article.urlToImage} alt='acticlePoster' />
+      <div className={classes.articlesCardImgContainer}>
+        <img className={classes.articlesCardImg} src={article.urlToImage} alt='acticlePoster' />
       </div>
 
-      <div className={classes.content}>
-        <div className={classes.publishedDateContainer}>
-          <CalendarTodayOutlinedIcon sx={{ fontSize: 14 }} />
-          <p className={classes.publishedDate}>{formatPublishedDate(article.publishedAt)}</p>
+      <div className={classes.articlesCardContent}>
+        <ArticlePublishedDate date={article.publishedAt} />
+
+        <div className={classes.articlesCardMainContent}>
+          <h2 className={classes.articlesCardTitle}>
+            <Highlighter
+              searchWords={keywords}
+              autoEscape={true}
+              textToHighlight={formatTextToFixedLength(article.title, MAX_TEXT_LENGTH)}
+            />
+          </h2>
+
+          <div className={classes.articlesCardDescription}>
+            <Highlighter
+              searchWords={keywords}
+              autoEscape={true}
+              textToHighlight={formatTextToFixedLength(article.description, MAX_TEXT_LENGTH)}
+            />
+          </div>
         </div>
 
-        <h2 className={classes.title}>
-          <Highlighter 
-            searchWords={keywords}
-            autoEscape={true}
-            textToHighlight={normilizeTextToFixedCharsNumber(article.title, 100)}
-          />
-        </h2>
-
-        <div className={classes.description}>
-          <Highlighter 
-            searchWords={keywords}
-            autoEscape={true}
-            textToHighlight={normilizeTextToFixedCharsNumber(article.description, 100)}
-          />
-        </div>
-
-        <div className={classes.navLinkContainer}>
-          <ArrowLink text='Read more' redirectURL='#'/>
-        </div>
+        <ArrowLink text='Read more' redirectURL='#' />
       </div>
     </div>
   );
