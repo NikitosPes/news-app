91cab5aa7265fa3dbb0bb2c4ffb20eb7d6b28140
NikitosPes <kutsokon95@gmail.com>
2023-01-20 11:18:58 +0300

    ref(LoadingSpiner): change html structure adn styles

--- src/components/LoadingSpiner/LoadingSpiner.tsx
+++ src/components/LoadingSpiner/LoadingSpiner.tsx
@@ -3,8 +3,10 @@ import classes from './LoadingSpiner.module.scss';
 
 const LoadingSpinner = () => {
   return (
-    <div className={classes.loadingSpinnerContainer}>
-      <div className={classes.loadingSpinner}></div>
+    <div className={classes.loadingSpinnerWrapper}>
+      <div className={classes.loadingSpinnerContainer}>
+        <div className={classes.loadingSpinner}></div>
+      </div>
     </div>
   );
 };
